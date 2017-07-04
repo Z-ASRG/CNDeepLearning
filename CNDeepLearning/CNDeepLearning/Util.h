38a660ec7a282e74748ea190df761018652174bf
zxyinz <zxyinz@gmail.com>
2017-07-04 14:32:25 -0400

    [抠鼻]

---
+++ CNDeepLearning/CNDeepLearning/Util.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include "iostream"
+#include "vector"
+#include "list"
+#include "set"
+#include "map"
+#include "unordered_set"
+#include "unordered_map"
+#include "thread"
+#include "algorithm"
+#include "cmath"
+#include "string"
+#include "ctime"
+#include "random"
+
+using namespace std;
+
+namespace ASRG
+{
+	namespace ML
+	{
+		typedef char				int8;
+		typedef unsigned char		uint8;
+		typedef short				int16;
+		typedef unsigned short		uint16;
+		typedef int					int32;
+		typedef unsigned int		uint32;
+		typedef long long			int64;
+		typedef unsigned long long	uint64;
+
+		typedef float				float32;
+		typedef double				float64;
+	}
+}
+
+#endif
